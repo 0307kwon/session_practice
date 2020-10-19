768f5a29c4d3b6b7da59a5d4ac0d18ab5a00809d
권세진 <0307kwon@gmail.com>
2020-10-19 21:31:38 +0900

    로그인 페이지 만들기

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const session = require("express-session");
+const body_parser = require("body-parser");
 
 const app = express();
+
+app.use(body_parser.urlencoded({extended: false}))
 
 app.use(session({
     secret: "dskvnDT12#$#!@",
@@ -9,14 +12,58 @@ app.use(session({
     saveUninitialized: true,
 }));
 
-app.get("/",(req,res)=>{
-    if(req.session.count !== undefined){
-        req.session.count++;
-        
+app.get("/auth/logout", (req,res)=>{
+    req.session.nickname = undefined;
+    res.redirect("/auth/login/page");
+});
+
+app.get("/welcome",(req,res)=>{
+    if(req.session.nickname !== undefined){
+        res.send(`
+            <body>
+                <h1>welcome</h1>
+                <p>안녕하세요 ${req.session.nickname} 님!</p>
+                <a href="/auth/logout">Logout</a>
+            </body>
+        `)
     }else{
-        req.session.count = 0;
+        res.send(`
+            <body>
+                <h1>welcome</h1>
+                <a href="/auth/login/page">Login</a>
+            </body>
+        `)
     }
-    res.send(`count : ${req.session.count}`);
+});
+
+app.post("/auth/login/process",(req,res)=>{
+    console.log(req.body);
+    const db = {
+        id:"0307kwon",
+        password:"1234",
+        nickname:"감군",
+    }
+    const id = req.body.id;
+    const pwd = req.body.password;
+    if(id === db.id && pwd === db.password){
+        req.session.nickname = db.nickname;
+        res.redirect("/welcome");
+    }else{
+        res.send("로그인 실패");
+    }
+});
+
+app.get("/auth/login/page",(req,res)=>{
+    res.send(`
+    <body>
+        <h1>Login</h1>
+        <form action="/auth/login/process" method="POST">
+            <p><input type="text" name="id" placeholder="id"></p>
+            <p><input type="password" name="password"></p>
+            <input type="submit">
+        </form>
+    </body>
+    `);
 })
 
 app.listen(3002,()=>{
