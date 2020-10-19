bd7e37560920df8b41acd5be56f5603a1f722235
권세진 <0307kwon@gmail.com>
2020-10-19 20:40:18 +0900

    세션 테스트

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const session = require("express-session");
+
 const app = express();
 
+app.use(session({
+    secret: "dskvnDT12#$#!@",
+    resave: false,
+    saveUninitialized: true,
+}));
+
 app.get("/",(req,res)=>{
-    res.send("안녕하세요");
+    if(req.session.count !== undefined){
+        req.session.count++;
+        
+    }else{
+        req.session.count = 0;
+    }
+    res.send(`count : ${req.session.count}`);
 })
 
 app.listen(3002,()=>{
