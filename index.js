71eb516fc6b642ab50610bf605e3bd95c5b47246
권세진 <0307kwon@gmail.com>
2020-10-22 21:57:40 +0900

    계정 생성, 로그인 기능 완성

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ mongoose.connect(`mongodb+srv://0307kwon:12345@cluster0.etajt.mongodb.net/test?r
     useUnifiedTopology: true,
 });
 
+const userSchema = new mongoose.Schema({
+    id:String,
+    pwd:String,
+    nickname:String,
+});
+
+const User = mongoose.model("User",userSchema);
+
 app.use(body_parser.urlencoded({extended: false}))
 
 app.use(session({
@@ -46,30 +54,61 @@ app.get("/welcome",(req,res)=>{
         res.send(`
             <body>
                 <h1>welcome</h1>
-                <a href="/auth/login/page">Login</a>
+                <p><a href="/auth/login/page">Login</a></p>
+                <p><a href="/auth/register/page">Register</a></p>
             </body>
         `)
     }
 });
 
-app.post("/auth/login/process",(req,res)=>{
-    console.log(req.body);
-    const db = {
-        id:"0307kwon",
-        password:"1234",
-        nickname:"감군",
-    }
+app.get("/auth/register/page",(req,res)=>{
+    res.send(`
+    <body>
+        <h1>Register</h1>
+        <form action="/auth/register/process"method="POST">
+            <p><input type="text" name="id" placeholder="id"></p>
+            <p><input type="password" name="pwd"></p>
+            <p><input type="text" name="nickname" placeholder="nickname"></p>
+            <p><input type="submit"></p>
+        </form>
+    </body>
+    `)
+})
+
+app.post("/auth/register/process",(req,res)=>{
     const id = req.body.id;
-    const pwd = req.body.password;
-    if(id === db.id && pwd === db.password){
-        req.session.nickname = db.nickname;
-        req.session.save((err)=>{
-            if(err) throw err;
-            res.redirect("/welcome");
-        });
-    }else{
-        res.send("로그인 실패");
-    }
+    const pwd = req.body.pwd;
+    const nickname = req.body.nickname;
+    const user = new User({
+        id:id,
+        pwd:pwd,
+        nickname:nickname,
+    });
+    user.save((err)=>{
+        if(err) throw err;
+        res.redirect("/welcome");
+    })
+
+
+});
+
+app.post("/auth/login/process",(req,res)=>{
+    User.findOne({id:req.body.id},(err,user)=>{
+        if(err) throw err;
+        if(user === null){
+            res.send("없는 아이디 입니다.");
+        }else{
+            if(user.pwd === req.body.pwd){
+                req.session.nickname = user.nickname;
+                req.session.save((err)=>{
+                    if(err) throw err;
+                    res.redirect("/welcome");
+                });
+            }else{
+                res.send("비밀번호 틀림");
+            }
+        }
+    });
 });
 
 app.get("/auth/login/page",(req,res)=>{
@@ -78,7 +117,7 @@ app.get("/auth/login/page",(req,res)=>{
         <h1>Login</h1>
         <form action="/auth/login/process" method="POST">
             <p><input type="text" name="id" placeholder="id"></p>
-            <p><input type="password" name="password"></p>
+            <p><input type="password" name="pwd"></p>
             <input type="submit">
         </form>
     </body>
